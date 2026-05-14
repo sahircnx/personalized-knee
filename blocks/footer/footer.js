f7eda70572479416e38188bbecb07c736786af26
Sahir khan <sahir.khan@concentrix.com>
2026-05-14 20:05:09 +0530

    test footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -187,11 +187,11 @@ function buildFromSections(sections) {
 }
 
 export default async function decorate(block) {
-  // const footerMeta = getMetadata('footer');
-  // const footerPath = footerMeta
-  //   ? new URL(footerMeta, window.location).pathname
-  //   : '/footer';
-  // const fragment = await loadFragment(footerPath);
+  const footerMeta = getMetadata('footer');
+  const footerPath = footerMeta
+    ? new URL(footerMeta, window.location).pathname
+    : '/footer';
+  const fragment = await loadFragment(footerPath);
 
   // block.textContent = '';
   // const sections = [...fragment.querySelectorAll('.section')];
